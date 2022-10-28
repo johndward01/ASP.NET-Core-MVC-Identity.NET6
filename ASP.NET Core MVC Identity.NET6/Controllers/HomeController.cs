9973713170bea8ed7a947c4622ff67c506bf40a1
johndward01 <johndward01@gmail.com>
2022-10-27 21:34:38 -0500

    added ThreeJsDemo for pointerlock tests updating home page working on navbar

--- ASP.NET Core MVC Identity.NET6/Controllers/HomeController.cs
+++ ASP.NET Core MVC Identity.NET6/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult ThreeJsDemo()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
