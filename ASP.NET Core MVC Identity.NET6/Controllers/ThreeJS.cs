0123c9c5e9af55022c0840453f7574c8687c2014
johndward01 <johndward01@gmail.com>
2022-11-22 18:28:31 -0600

    moved 3js to it's own page

---
+++ ASP.NET Core MVC Identity.NET6/Controllers/ThreeJS.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_MVC_Identity.NET6.Controllers;
+public class ThreeJS : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
