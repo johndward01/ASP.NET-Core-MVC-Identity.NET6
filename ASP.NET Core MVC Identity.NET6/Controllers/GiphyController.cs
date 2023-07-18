1cff4b8fb713b640520560589c6e6197ccb597ee
johndward01 <johndward01@gmail.com>
2023-07-17 20:36:06 -0500

    used _config to grab key

--- ASP.NET Core MVC Identity.NET6/Controllers/GiphyController.cs
+++ ASP.NET Core MVC Identity.NET6/Controllers/GiphyController.cs
@@ -1,19 +1,23 @@
 ﻿using ASP.NET_Core_Identity_Demo.GiphyAPI;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Net.Http;
-using System.Threading;
 
 namespace ASP.NET_Core_Identity_Demo.Controllers
 {
     public class GiphyController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public GiphyController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
-        {            
+        {
+            string apiKey = _configuration.GetConnectionString("Giphy");
             var client = new HttpClient();
-            var url = "https://api.giphy.com/v1/gifs/search?api_key=IVv33It9Q4JM0mn8AfKlwxfLwkkY28yx&q=cat+meme&limit=25&offset=0&rating=pg-13&lang=en";
+            var url = $"https://api.giphy.com/v1/gifs/search?api_key={apiKey}&q=cat+meme&limit=25&offset=0&rating=pg-13&lang=en";
             var response = client.GetStringAsync(url).Result;
             var root = JsonConvert.DeserializeObject<GiphyAPI_Root>(response);
             return View(root);
